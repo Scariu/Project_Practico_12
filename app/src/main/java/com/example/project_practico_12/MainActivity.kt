43a50ff154b2a048a256a8a57b89b773070c1100
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-14 17:59:56 -0400

    Se agregan validaciones a nombre, apellido, edad, y sistemaSalud. Pendiente areglar validación de correo.

--- app/src/main/java/com/example/project_practico_12/MainActivity.kt
+++ app/src/main/java/com/example/project_practico_12/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.project_practico_12
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns.EMAIL_ADDRESS
+import androidx.appcompat.app.AppCompatActivity
+import java.util.regex.Pattern
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,46 +13,109 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-fun main(){
+
+
+
+fun main() {
+
+    var nombre: String
+    var apellido: String
+    var edad: Int
+    var correo: String
+    var sistemaSalud: String
+
     val usuarios = mutableListOf<Usuario>()
     println("Ingrese la cantidad de usuarios: ")
-    val cantidadUsuario = readLine()!!.toInt()
+    var cantidadUsuario = readLine()!!.toInt()
 
-    for(i in 1..cantidadUsuario){
+    for (i in 1..cantidadUsuario) {
         //NOMBRE
         println("Ingrese nombre: ")
-        var nombre = readln()
-
+        nombre = readln()
+        while (!validarNombre(nombre)) {
+            println("Nombre inválido, ingrese un nombre válido.")
+            nombre = readln()
+        }
         //APELLIDO
         println("Ingrese apellido: ")
-        val apellido = readln()
+        apellido = readln()
+        while (!validarApellido(apellido)) {
+            println("Apellido inválido, ingrese un apellido válido.")
+            apellido = readln()
+        }
 
         //EDAD
         println("Ingrese edad: ")
-        val edad = readln().toInt()
+        edad = readln().toInt()
+        while (!validarEdad(edad)) {
+            println("Edad inválido, ingrese una edad válida.")
+            edad = readln().toInt()
+        }
 
         //CORREO
         println("Ingrese correo electrónico: ")
-        val correo = readln()
+        correo = readln()
+        while (validarCorreo(correo)) {
+            println("Correo inválido, ingrese un correo válido.")
+            apellido = readln()
+        }
 
         //SISTEMA SALUD
-        println("Seleccione su sistema de salud (seleccione el número):")
+        println("Seleccione su sistema de salud (indique el número):")
         println("1.Fonasa")
         println("2.Isapre")
-        val sistemaSalud = readln().toInt()
+        println("3.Particular")
+        sistemaSalud = readln()
+            while (!validarSistema(sistemaSalud)){
+                println("Correo inválido, ingrese un correo válido.")
+                sistemaSalud = readln()
+            }
 
 
-        //val sistemaSalud = when
-
-        val usuario = Usuario(nombre, apellido, edad, correo, sistemaSalud)
+        var usuario = Usuario(nombre, apellido, edad, correo, sistemaSalud)
         usuarios.add(usuario)
-    }
-    for (u in usuarios){
-        println(u)
+
+        for (u in usuarios) {
+            println(u)
+        }
     }
 }
 
 
 
 //DataClass
-data class Usuario(val nombre: String, val apellido: String, val edad: Int, val correo: String, val sistemaSalud: Int)
+data class Usuario(
+    val nombre: String,
+    val apellido: String,
+    val edad: Int,
+    val correo: String,
+    val sistemaSalud: String
+)
+
+    fun validarSistema(sistemaSalud: String): Boolean {
+        when (sistemaSalud) {
+            "1" -> println("Seleccionó Fonasa")
+            "2" -> println("Seleccionó Isapre")
+            "3"-> println("Seleccionó Particular")
+        }
+        return sistemaSalud.equals("1") || sistemaSalud.equals("2") || sistemaSalud.equals("3")
+    }
+fun validarEdad(edad: Int): Boolean {
+    return edad in 1..115
+}
+
+fun validarCorreo(correo: String): Boolean {
+    return correo.all { it.isLetterOrDigit() } && correo.contains(Regex("^\\S+@\\S+\\.\\S+$"))
+}
+
+
+fun validarApellido(apellido: String): Boolean {
+    return apellido.all { it.isLetter() }
+
+}
+
+fun validarNombre(nombre: String): Boolean {
+    return nombre.length in 1..20 && nombre.all { it.isLetter() }
+
+}
+
